097211635d4d9b67a534579376335dcb738c1764
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-04-02 11:47:58 +0100

    Initial commit

---
+++ using_lists_in_a_function.py
@@ -0,0 +1,26 @@
+# l = [1, 5, 6, 6, 7, 7, 8]
+#
+#
+# def print_list(x):
+#     for i in range(0, len(x)):
+#         print x[i]
+#
+#
+# print print_list(l)
+#
+# print
+# print "Let's append some more values to our List!"
+# print
+#
+# l.append([4, 5])
+#
+# print l
+
+# print
+
+our_list = list(range(10))
+print our_list
+
+our_list + [10, 11, 12]
+
+print our_list
