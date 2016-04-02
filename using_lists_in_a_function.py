3bf640eda184a009ef35a26bda5e60187c955c2e
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-04-02 21:47:17 +0100

    test list(range(n))

--- using_lists_in_a_function.py
+++ using_lists_in_a_function.py
@@ -1,26 +1,25 @@
-# l = [1, 5, 6, 6, 7, 7, 8]
-#
-#
-# def print_list(x):
-#     for i in range(0, len(x)):
-#         print x[i]
-#
-#
-# print print_list(l)
-#
-# print
-# print "Let's append some more values to our List!"
-# print
-#
-# l.append([4, 5])
-#
-# print l
+l = [1, 5, 6, 6, 7, 7, 8]
 
-# print
+
+def print_list(x):
+    for i in range(0, len(x)):
+        print x[i]
+
+
+print print_list(l)
+
+print
+print "Let's append some more values to our List!"
+print
+
+l.append([4, 5])
+
+print l
+
+print
 
 our_list = list(range(10))
 print our_list
 
-our_list + [10, 11, 12]
 
-print our_list
+
