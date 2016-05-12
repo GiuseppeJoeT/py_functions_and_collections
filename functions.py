fb46e18151e0140a76eec92b2c3ed19150d19f50
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-05-12 13:52:01 +0200

    Updated functions.py - added print_one_arg() function

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@ def print_two(*args):
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
+
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
     print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+
+def print_one_arg(arg1):
+    print "arg1: %r" % arg1
